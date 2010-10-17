c7d157ee4017afe3f14813953d1fc6885e7e2389
Ian Gallagher <crash@neg9.org>
2010-10-17 16:19:18 -0700

    Improve checking for/fetching Google profile image  * Check if DOM element exists instead of just wrapping in try/catch

--- xpi/handlers/google.js
+++ xpi/handlers/google.js
@@ -13,19 +13,17 @@ register({
 
   identifyUser: function() {
     var resp = this.httpGet(this.siteUrl);
-    this.userName = resp.body.querySelectorAll(".gb4")[0].textContent;
+    this.userName = resp.body.querySelector(".gb4").textContent;
 
     // Grab avatar from Google Profiles page, if they have one
-    try {
-	    var profile = this.httpGet('http://www.google.com/profiles/me');
-	    this.userAvatar = profile.body.querySelector('.ll_profilephoto.photo').src;
+    var profile = this.httpGet('http://www.google.com/profiles/me');
+    var avatar_element = profile.body.querySelector('.ll_profilephoto.photo');
+
+    if (avatar_element) {
+	    this.userAvatar = avatar_element.src;
 	    if (this.userAvatar.substr(0, 4) != 'http') {
 	      this.userAvatar = 'http://www.google.com' + this.userAvatar;
 	    }
-    }
-    catch(err) {
-	    // Something went wrong - most likley user does not have a Google Profile,
-	    // simply don't set the userAvatar in this case.
     }
   }
 });
