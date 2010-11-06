27e0d43acced2f0e3db31fd48467e253e6146eac
Nick Kossifidis <mickflemm@gmail.com>
2010-11-06 23:39:23 +0200

    * In case pcap doesn't provide us with a decription for an interface use info.product from HAL

--- backend/src/linux_platform.cpp
+++ backend/src/linux_platform.cpp
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <pcap/pcap.h>
 #include "linux_platform.hpp"
+#include <libhal.h>
 
 
 LinuxPlatform::LinuxPlatform(vector<string> argv) : UnixPlatform(argv) { }
@@ -34,32 +35,107 @@ bool LinuxPlatform::run_privileged() {
 	return true;
 }
 
+static char*
+get_hal_interface_description(char* ifname)
+{
+	int num_udis;
+	char **udis;
+	const char *key = NULL;
+	char *value = NULL;
+	DBusError error;
+	LibHalContext *hal_ctx;
+
+	/* Initialize dbus connection */
+	dbus_error_init (&error);	
+	if ((hal_ctx = libhal_ctx_new()) == NULL) {
+		fprintf(stderr, "error: libhal_ctx_new\n");
+		return NULL;
+	}
+	if (!libhal_ctx_set_dbus_connection(hal_ctx,
+	dbus_bus_get(DBUS_BUS_SYSTEM, &error))) {
+		fprintf(stderr,
+			"error: libhal_ctx_set_dbus_connection: %s: %s\n",
+			error.name, error.message);
+		LIBHAL_FREE_DBUS_ERROR (&error);
+		return NULL;
+	}
+	/* Initialize hal context */
+	if (!libhal_ctx_init (hal_ctx, &error)) {
+		if (dbus_error_is_set(&error)) {
+			fprintf(stderr,
+				"error: libhal_ctx_init: %s: %s\n",
+				error.name, error.message);
+			dbus_error_free (&error);
+		}
+		fprintf(stderr,
+			"Could not initialise connection to hald.\n"
+			"Is hald running ?\n");
+		return NULL;
+	}
+
+
+	/* Search device uid by property */
+	key="net.interface";
+	udis = libhal_manager_find_device_string_match (hal_ctx, key,
+						ifname, &num_udis, &error);
+
+	if (dbus_error_is_set (&error)) {
+		fprintf (stderr, "error: %s: %s\n",
+			error.name, error.message);
+		dbus_error_free (&error);
+		return NULL;
+	}
+
+	/* No devices found */
+	if (num_udis == 0) {
+		return NULL;
+	}
+
+	/* Get info.product for this device */
+	key="info.product";
+	value = libhal_device_get_property_string(hal_ctx, udis[0],
+							key, &error);
+	libhal_free_string_array (udis);
+
+	return value;
+}
+
 vector<InterfaceInfo> LinuxPlatform::interfaces()
 {
 	vector<InterfaceInfo> result;
 	char err_buff[PCAP_ERRBUF_SIZE];
+	const char *description;
+	const char *type;
 	pcap_if_t *all_devs;
 	if (pcap_findalldevs(&all_devs, err_buff) == 0) {
 		pcap_if_t *dev = all_devs;
 		while (dev) {
-			pcap_t *interface = pcap_open_live(dev->name, 1024 ,1, 100, err_buff);
+			pcap_t *interface = pcap_open_live(dev->name,
+							1024 ,1, 100,
+							err_buff);
 
 			if (interface == NULL) {
 				dev = dev->next;
 				continue;
 			}
 
-			if (pcap_datalink(interface) == DLT_EN10MB) {
-				const char *description = (dev->description) ? dev->description : dev->name;
-				InterfaceInfo info(dev->name, description, "ethernet");
-				result.push_back(info);
+			switch (pcap_datalink(interface)) {
+			case DLT_EN10MB:
+				type = "ethernet";
+				break;
+			case DLT_IEEE802_11_RADIO:
+				type = "802.11 monitor";
+				break;
+			default:
+				pcap_close(interface);
+				dev = dev->next;
+				continue;
 			}
 
-			if (pcap_datalink(interface) == DLT_IEEE802_11_RADIO) {
-				const char *description = (dev->description) ? dev->description : dev->name;
-				InterfaceInfo info(dev->name, description, "802.11 monitor");
-				result.push_back(info);
-			}
+			description = (dev->description) ? dev->description :
+					get_hal_interface_description(dev->name);
+			InterfaceInfo info(dev->name, description, type);
+			result.push_back(info);
 
 			pcap_close(interface);
 			dev = dev->next;
