d0ee55f15bba36f79f484b8948f240653075e5dc
Eric Butler <eric@Tin.local>
2011-07-25 22:32:37 -0700

    New Handlers: GroupMe, Quora. Updated Handlers: Google, StackOverflow.

--- xpi/handlers/stackoverflow.js
+++ xpi/handlers/stackoverflow.js
@@ -1,12 +1,17 @@
 // Authors:
 //   Nick Presta <nick@nickpresta.ca>
+//   Eric Butler <eric@codebutler.com>
+
+Components.utils.import('resource://firesheep/util/Utils.js');
 
 register({
-  name: "StackOverflow",
+  name: "Stack Overflow",
   domains: [ 'stackoverflow.com' ],
-  sessionCookieNames: [ 'usr', 'gauthed' ],
+  sessionCookieNames: [ 'usr' ],
   identifyUser: function () {
+    this.sessionId = Utils.parseQuery(this.firstPacket.cookies['usr'])['s'];
+  
     var resp = this.httpGet(this.siteUrl);
-    this.userName = resp.body.querySelectorAll('span#hlinks-user a')[1].textContent;
+    this.userName = resp.body.querySelector('span#hlinks-user a.profile-link').textContent;
   }
 });
