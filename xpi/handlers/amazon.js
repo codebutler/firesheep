68e6365c19298c1333daa8e31fdee01986e04acc
Pedro Nascimento <pnascimento@gmail.com>
2012-07-13 02:08:54 -0300

    Correctly fetch username from Amazon.

--- xpi/handlers/amazon.js
+++ xpi/handlers/amazon.js
@@ -9,7 +9,7 @@ register({
   identifyUser: function() {
     var resp = this.httpGet(this.siteUrl);
 
-    var newElem = this.userName = resp.body.querySelector("#nav-your-account .nav-button-title em");
+    var newElem = this.userName = resp.body.querySelector("#nav-your-account .nav-button-em");
     if(newElem != null)
       this.userName = newElem.textContent;
     else
