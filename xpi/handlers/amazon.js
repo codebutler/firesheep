090dfedcf8fe652cb6c53fd6c85f06c56729e356
Eric Butler <eric@codebutler.com>
2013-03-09 17:16:24 -0800

    Fix Amazon handler. Closes #173

--- xpi/handlers/amazon.js
+++ xpi/handlers/amazon.js
@@ -8,11 +8,6 @@ register({
 
   identifyUser: function() {
     var resp = this.httpGet(this.siteUrl);
-
-    var newElem = this.userName = resp.body.querySelector("#nav-your-account .nav-button-title em");
-    if(newElem != null)
-      this.userName = newElem.textContent;
-    else
-      this.userName = resp.body.querySelector(".navGreeting").textContent.split(', ')[1].slice(0, -1);
+    this.userName = resp.body.querySelector("#nav-signin-text").textContent;
   }
 });
